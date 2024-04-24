9d37757fe678c19f01c11a40e0f19ce5684d01ed
ashsoldier <153024446+ashsoldier@users.noreply.github.com>
2024-04-24 18:04:56 -0500

    2nd

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 console.log("Your index.js file is loaded correctly!");
-$("#target").on("click", function(){
+("#target").on("click", function(){
     alert("Handler for 'click' called.");
+    $(".fadeMe").fadeToggle("slow");
 });
 console.log("Making sure this step worked");
