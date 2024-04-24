af7857dd6256ae51fe3edf2128a3b0b352c90e4a
ashsoldier <153024446+ashsoldier@users.noreply.github.com>
2024-04-24 17:50:21 -0500

    Js trial

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 console.log("Your index.js file is loaded correctly!");
+$("#target").on("click", function(){
+    alert("Handler for 'click' called.");
+});
+console.log("Making sure this step worked");
