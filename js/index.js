2a6848536cdf3a1934742a793f7106ce6fb67b29
ashsoldier <153024446+ashsoldier@users.noreply.github.com>
2024-04-27 15:50:52 -0500

    Onclick retry

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-console.log("Your index.js file is loaded correctly!");
-("#target").on("click", function(){
-    alert("Handler for 'click' called.");
-    $(".fadeMe").fadeToggle("slow");
-});
-console.log("Making sure this step worked");
+//console.log("Your index.js file is loaded correctly!");
+//("#target").on("click", function(){
+//    alert("Handler for 'click' called.");
+//    $(".fadeMe").fadeToggle("slow");
+//});
+//console.log("Making sure this step worked");
+//This did not work. I tried to have my code scroll down once you clicked the button. I viewed many W3 videos and even scheduled a tutoring session, but we could not figure it out. 
+function thankYou() {
+    document.getElementById("thankYouNote").innerHTML = "Thank you for downloading my resume";
+}
