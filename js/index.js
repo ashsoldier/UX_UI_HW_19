4e682e24148fb4e0078191ac31ed9c5743b9be1b
ashsoldier <153024446+ashsoldier@users.noreply.github.com>
2024-04-27 16:00:10 -0500

    Fixed Code

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@
 //});
 //console.log("Making sure this step worked");
 //This did not work. I tried to have my code scroll down once you clicked the button. I viewed many W3 videos and even scheduled a tutoring session, but we could not figure it out. 
-function thankYou() {
-    document.getElementById("thankYouNote").innerHTML = "Thank you for downloading my resume";
+function myFunction() {
+    document.getElementById("demo").innerHTML="Thank you for downloading my resume";
 }
