860590faf4529f501fe3297edfd15c47eb8b2702
ashsoldier <153024446+ashsoldier@users.noreply.github.com>
2024-05-06 21:05:04 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//console.log("Your index.js file is loaded correctly!");
+console.log("Your index.js file is loaded correctly!");
 //("#target").on("click", function(){
 //    alert("Handler for 'click' called.");
 //    $(".fadeMe").fadeToggle("slow");
@@ -6,5 +6,8 @@
 //console.log("Making sure this step worked");
 //This did not work. I tried to have my code scroll down once you clicked the button. I viewed many W3 videos and even scheduled a tutoring session, but we could not figure it out. 
 function myFunction() {
+    console.log("right clicked");
     document.getElementById("demo").innerHTML="Thank you for downloading my resume";
+    var button = document.getElementById("Resume");
+    button.classList.add("green");
 }
